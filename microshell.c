4a691b8ce99f4d460d6013994c4b2252213f0793
Mohamed Haddi <m.haddi.95@gmail.com>
2022-04-16 23:10:42 +0000

    add more comments; make cd into a function

--- microshell.c
+++ microshell.c
@@ -6,7 +6,7 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 14:36:47 by mhaddi            #+#    #+#             */
-/*   Updated: 2022/04/16 22:20:07 by mhaddi           ###   ########.fr       */
+/*   Updated: 2022/04/16 23:08:07 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int count_pipe_seperated_cmds(int i_argv, int argc, char **argv);
 int get_command_size(int i_argv, int argc, char **argv);
+void cd(int cmd_size, char **args);
 void fatal();
 
 int main(int argc, char **argv, char **env)
@@ -50,12 +51,52 @@ int main(int argc, char **argv, char **env)
 			}
 			args[i] = NULL;
 
-			// pipes
+			/*
+			 * BEGIN PIPING
+			 */
+
+			/*
+			 * we're now ready to execve the current pipe-seperated command.
+			 * but first we need to create a pipe and set its FDs.
+			 * for that, I have used the following algorithm:
+			 *
+			 * ```
+			 * for cmd in cmds
+			 *     if there is a next cmd
+			 *         pipe(new_fds)
+			 *     fork
+			 *     if child
+			 *         if there is a previous cmd
+			 *             dup2(old_fds[0], 0)
+			 *             close(old_fds[0])
+			 *             close(old_fds[1])
+			 *         if there is a next cmd
+			 *             close(new_fds[0])
+			 *             dup2(new_fds[1], 1)
+			 *             close(new_fds[1])
+			 *         exec cmd || die
+			 *     else
+			 *         if there is a previous cmd
+			 *             close(old_fds[0])
+			 *             close(old_fds[1])
+			 *         if there is a next cmd
+			 *             old_fds = new_fds
+			 * if there are multiple cmds
+			 *     close(old_fds[0])
+			 *     close(old_fds[1])
+			 * ```
+			 *
+			 * this code avoids mangling the parent's stdin and stdout,
+			 * only manipulating them within the child, and never leaks any FDs
+			 * source: https://stackoverflow.com/a/917700/4273342
+			 *
+			 */
+
 			if (i_pipe_cmds != pipe_cmds_num - 1) // if there is a next command
 				if (pipe(new_fds) == -1) fatal(); // create pipe
 			pid_t pid = fork(); // fork
 			if (pid == -1) fatal();
-			if (pid == 0)
+			if (pid == 0) // child process
 			{
 				if (i_pipe_cmds != 0) // if there is a previous command
 				{
@@ -70,37 +111,22 @@ int main(int argc, char **argv, char **env)
 					close(new_fds[1]);
 				}
 				
-				if (strcmp(args[0], "cd") == 0)
+				// execute
+				if (strcmp(args[0], "cd")) // if not cd, execve
 				{
-					if (cmd_size != 2)
+					if (execve(args[0], args, env) == -1)
 					{
-						write(2, "error: cd: bad arguments\n", 25);
-						exit(1);
-					}
-					else
-					{
-						if (chdir(args[1]) == -1)
-						{
-							write(2, "error: cd: cannot change directory to ", 38);
-							write(2, args[1], strlen(args[1]));
-							write(2, "\n", 1);
-							exit(1);
-						}
-						else
-							exit(0);
+						write(2, "error: cannot execute ", 22);
+						write(2, args[0], strlen(args[0]));
+						write(2, "\n", 1);
+						exit(1); // exit process with error
 					}
 				}
-				else if (execve(args[0], args, env) == -1)
-				{
-					write(2, "error: cannot execute ", 22);
-					write(2, args[0], strlen(args[0]));
-					write(2, "\n", 1);
-					exit(1);
-				}
+				else cd(cmd_size, args); // otherwise, chdir
 			}
-			else
+			else // parent process
 			{
-				pids[i_pipe_cmds] = pid;
+				pids[i_pipe_cmds] = pid; // save pid
 				if (i_pipe_cmds != 0) // if there is a previous command
 				{
 					close(old_fds[0]);
@@ -113,6 +139,10 @@ int main(int argc, char **argv, char **env)
 				}
 			}
 
+			/*
+			 * END PIPING
+			 */
+
 			i_argv++; // escape the "|" or ";" argument (this is where we left off after copying from argv to args)
 			i_pipe_cmds++; // continue to next pipe-seperated command
 		}
@@ -123,14 +153,34 @@ int main(int argc, char **argv, char **env)
 			close(old_fds[1]);
 		}
 
-		// wait for all child processes to finish
-		for (int i = 0; i < pipe_cmds_num; i++)
+		for (int i = 0; i < pipe_cmds_num; i++) // wait for all child processes to finish
 			if (waitpid(pids[i], NULL, 0) == -1) fatal();
 
 		// continue to next semicolon-seperated command
 	}
 
 	return (0);
+}
+
+void cd(int cmd_size, char **args)
+{
+	if (cmd_size != 2) // shouldn't be more or less than 2 args
+	{
+		write(2, "error: cd: bad arguments\n", 25);
+		exit(1); // exit process with error
+	}
+	else
+	{
+		if (chdir(args[1]) == -1) // change directory
+		{
+			write(2, "error: cd: cannot change directory to ", 38);
+			write(2, args[1], strlen(args[1]));
+			write(2, "\n", 1);
+			exit(1); // exit process with error
+		}
+		else
+			exit(0); // exit process with success
+	}
 }
 
 int count_pipe_seperated_cmds(int i_argv, int argc, char **argv)
